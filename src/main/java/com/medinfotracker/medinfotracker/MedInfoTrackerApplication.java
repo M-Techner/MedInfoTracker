109892bfb3f143fb0492c5e5ab5af2de889cdbeb
Mags8934 <84347132+Mags8934@users.noreply.github.com>
2021-12-06 20:22:12 -0600

    first commit

---
+++ src/main/java/com/medinfotracker/medinfotracker/MedInfoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.medinfotracker.medinfotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedInfoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedInfoTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medinfotracker/medinfotracker/MedInfoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinfotracker.medinfotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedInfoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
