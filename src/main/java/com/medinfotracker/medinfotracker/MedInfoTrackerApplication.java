f2cc4784fcf8ca73bdae53f04d6665d4edb9ddda
aspiezebra <aspiezebra@gmail.com>
2021-12-13 19:13:16 -0600

    add

--- src/main/java/com/medinfotracker/medinfotracker/MedInfoTrackerApplication.java
+++ src/main/java/com/medinfotracker/medinfotracker/MedInfoTrackerApplication.java
@@ -1,7 +1,18 @@
 package com.medinfotracker.medinfotracker;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.*;
+import java.util.*;
+
 
 @SpringBootApplication
 public class MedInfoTrackerApplication {
