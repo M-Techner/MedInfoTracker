ed07b7ef2e278b383c036069aa74a33bc009755c
aspiezebra <aspiezebra@gmail.com>
2022-01-17 19:36:49 -0600

    new

--- src/main/java/com/medinfotracker/medinfotracker/controllers/SymptomController.java
+++ src/main/java/com/medinfotracker/medinfotracker/controllers/SymptomController.java
@@ -5,18 +5,31 @@ import com.medinfotracker.medinfotracker.models.data.SymptomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("symptom")
 public class SymptomController {
 
+    /**
+     *
+     */
     @Autowired
     private SymptomRepository symptomRepository;
 
 
     @GetMapping("")
     public String index(Model model){
+        model.addAttribute("Symptom Name", "sxName");
+        model.addAttribute("Symptom Description", "sxDescribe");
 
-   }
+        return "/index";
+    }
+
+
 }
