f25ed216dca4d86dbe261405ba3371c07abfc5ec
aerhufker <96671542+aerhufker@users.noreply.github.com>
2022-02-03 19:05:09 -0600

    add

--- src/main/java/com/medinfotracker/medinfotracker/controllers/SearchController.java
+++ src/main/java/com/medinfotracker/medinfotracker/controllers/SearchController.java
@@ -3,6 +3,7 @@ package com.medinfotracker.medinfotracker.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("search")
 public class SearchController {
