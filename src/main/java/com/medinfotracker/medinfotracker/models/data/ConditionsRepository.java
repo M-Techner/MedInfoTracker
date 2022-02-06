f4c1b36ef4b14cb4581906f5697c3d95e75a9268
aerhufker <96671542+aerhufker@users.noreply.github.com>
2022-02-06 17:00:51 -0600

    Add files via upload

---
+++ src/main/java/com/medinfotracker/medinfotracker/models/data/ConditionsRepository.java
@@ -0,0 +1,12 @@
+
+package com.medinfotracker.medinfotracker.models.data;
+
+import com.medinfotracker.medinfotracker.models.Conditions;
+
+import java.util.Optional;
+
+public interface ConditionsRepository {
+//    void save(Conditions newConditions);
+//
+//    Optional findById(int conditionsId);
+}
