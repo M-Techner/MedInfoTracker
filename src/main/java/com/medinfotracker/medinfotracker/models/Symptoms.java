1e8c92601286deb06944cfdd2be6092339c3e25c
aspiezebra <aspiezebra@gmail.com>
2022-02-21 11:25:05 -0600

    fix typo

--- src/main/java/com/medinfotracker/medinfotracker/models/Symptoms.java
+++ src/main/java/com/medinfotracker/medinfotracker/models/Symptoms.java
@@ -42,7 +42,7 @@ public class Symptoms extends AbstractEntity {
     }
 
 //    @Override
-    pit sublic void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
