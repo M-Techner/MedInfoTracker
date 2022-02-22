6b788da1ea83ba7a97cc4b92ae9367494e1c3bf2
aerhufker <96671542+aerhufker@users.noreply.github.com>
2022-02-22 15:04:37 -0600

    Update UserRepository.java

--- src/main/java/com/medinfotracker/medinfotracker/models/data/UserRepository.java
+++ src/main/java/com/medinfotracker/medinfotracker/models/data/UserRepository.java
@@ -1,18 +1,13 @@
 package com.medinfotracker.medinfotracker.models.data;
 
-<<<<<<< HEAD
+
 import com.medinfotracker.medinfotracker.models.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
-=======
-
 import com.medinfotracker.medinfotracker.models.User;
 import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
->>>>>>> e68f6f5f273a59d08c301141edfeaab44c0081a2
 
 @Repository
 @Transactional
